9a30d131b710c3cba7e8632d4432e00c6ccf9b8a
kao_jun <863646260@qq.com>
2023-11-16 14:18:55 +0800

    音乐游戏

---
+++ BIG-Game/Assets/script/ForMusic/MusicRhythmGenerator.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicRhythmGenerator : MonoBehaviour
+{
+    public GameObject objectToSpawn; // 要生成的游戏物体
+    public AudioSource audioSource; // 音乐源
+    public float sensitivity = 2.0f; // 敏感度，用于调整节拍检测的灵敏度
+    public float spawnDelay = 0.1f; // 生成物体的延迟时间
+    public float spawnInterval = 0.5f; // 生成物体的间隔时间
+    public float threshold = 0.01f; // 频谱数据阈值，用于判断是否为节拍
+
+    private float[] _spectrumData = new float[8192];
+    private float _lastSpawnTime = -1.0f;
+    private float _beatTimer = 0f;
+
+    void Start()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+    }
+
+    void Update()
+    {
+        // 获取频谱数据
+        audioSource.GetSpectrumData(_spectrumData, 0, FFTWindow.BlackmanHarris);
+
+        // 分析频谱数据来检测节拍
+        float maxSpectrumValue = 0f;
+        int maxSpectrumIndex = 0;
+        for (int i = 0; i < _spectrumData.Length; i++)
+        {
+            if (_spectrumData[i] > maxSpectrumValue && _spectrumData[i] > threshold)
+            {
+                maxSpectrumValue = _spectrumData[i];
+                maxSpectrumIndex = i;
+            }
+        }
+
+        // 如果检测到节拍
+        if (maxSpectrumValue > sensitivity)
+        {
+            _beatTimer += Time.deltaTime;
+            if (_beatTimer > spawnInterval)
+            {
+                // 生成物体
+                StartCoroutine(SpawnObjectWithDelay(spawnDelay));
+                _beatTimer = 0f;
+            }
+        }
+    }
+
+    private IEnumerator SpawnObjectWithDelay(float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Instantiate(objectToSpawn, transform.position, Quaternion.identity);
+    }
+}
---
+++ BIG-Game/Assets/script/ForMusic/RandomPosition.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class RandomPosition : MonoBehaviour
+{
+    public Vector3[] positions; // 存储四个位置坐标
+
+    private void Start()
+    {
+      
+        // 随机选择一个位置坐标
+        int randomIndex = Random.Range(0, 4);
+        Vector3 randomPosition = positions[randomIndex];
+
+        // 将物体移动到随机选择的位置坐标
+        transform.position = randomPosition;
+    }
+}
