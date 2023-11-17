cb96f7a7fdd11d8a3c6e8ab020dead01497cb0e8
aidendenden233 <2996876880@qq.com>
2023-11-18 02:11:24 +0800

    tweak

---
+++ BIG-Game/Assets/script/whack-a-mole/Level.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System;
+
+
+/// <summary>
+/// 此脚本定义游戏中的一个关卡。当玩家达到一定分数时，等级会提高，难度也会相应改变
+/// </summary>
+[Serializable]
+public class Level
+{
+    [Tooltip("The score needed to win this level")]
+    public int scoreToNextLevel = 200;
+
+    [Tooltip("The time bonus for finishing this level")]
+    public float timeBonus = 5;
+
+    [Tooltip("The maximum number of targets allowed on screen at once")]
+    public int maximumTargets = 2;
+}
